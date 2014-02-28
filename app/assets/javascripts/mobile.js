83303a1b115136f8f4cf08e7468647572f5585d6
Sebastien Renault <sre@zenexity.com>
2014-02-28 11:00:48 +0100

    wip web socket mobile part

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,3 +1,41 @@
 /**
  * mobile.js
  */
+
+(function() {
+    var ZenFeedBack = function(streamURL, init) {
+        var self = this;
+        var socket = new WebSocket(streamURL);
+
+        socket.onopen = function() {
+            init(socket);
+        };
+    };
+
+    var Stream = function(socket) {
+        this.socket = socket;
+
+        this.socket.onmessage = function onmessage(event) {
+            console.log(event.data);
+        };
+    };
+
+    Stream.prototype.send = function(data) {
+        this.socket.send(JSON.stringify(data));
+    };
+
+    // Let's go
+    var roomName = $('body').attr('data-room');
+    window.zenFeedBack = new ZenFeedBack(
+        'ws://localhost:9000/rooms/' + roomName + '/listen',
+        function init(socket) {
+            window.zStream = new Stream(socket);
+        }
+    );
+
+})();
+
+$(document).ready(function() {
+    console.log('Welcome to ZenFeedBack !');
+    window.zStream.send({ msg: 'ping!'});
+});
